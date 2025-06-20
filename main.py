32f58ffcf24dfa850393ff79e60d49f865b8df11
Ken Rolloque <Ken.Rolloque@fusionnode.ai>
2025-06-20 13:51:07 +0800

    add header

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, Depends, status, Header
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from config import settings
-from typing import Any
+from typing import Any, Optional
 
 app = FastAPI()
 security = HTTPBearer()
 
 @app.post("/api/v2/postAlert")
-async def postAlert(data:dict, credentials: HTTPAuthorizationCredentials = Depends(security)):
+async def postAlert(
+        data:dict, 
+        credentials: HTTPAuthorizationCredentials = Depends(security),
+        user_agent: Optional[str] = Header(None, alias="User-Agent"),
+        netskope_datatype: Optional[str] = Header(None, alias="X-Netskope-DataType"),
+        netskope_subtype: Optional[str] = Header(None, alias="X-Netskope-Subtype"),
+        
+        ):
     try:
         token = credentials.credentials
         if token != settings.bearer_token:
