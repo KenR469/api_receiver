82bfc68c256e5f12a16166da9e337d540afebb97
Ken Rolloque <Ken.Rolloque@fusionnode.ai>
2025-06-23 14:16:03 +0800

    Updated

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 from fastapi import FastAPI, HTTPException, Depends, status, Header, Request
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from config import settings
-from typing import Any, Optional, List
-import pprint
+from typing import Any, Optional
 
 app = FastAPI()
 security = HTTPBearer()
 
 @app.post("/api/v2/postAlert")
 async def postAlert(
-        payload: List[Any],
+        request: Request,
         credentials: HTTPAuthorizationCredentials = Depends(security),
         user_agent: Optional[str] = Header(None, alias="User-Agent"),
         netskope_datatype: Optional[str] = Header(None, alias="X-Netskope-DataType"),
@@ -17,20 +16,13 @@ async def postAlert(
         
         ):
     try:
-        print("=" * 80)
-        print("Received new data from Netskope Plugin")
-        print("-" * 80)
-        
-        # Pretty-print the JSON payload
-        print("Payload Content:")
-        pprint.pprint(payload)
-        
-        print("=" * 80 + "\n")
-        
-        return {
-            "status": "success",
-            "message": f"Successfully received {len(payload)} records."
-        }
+        token = credentials.credentials
+        if token != settings.bearer_token:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
+    
+        raw_body = await request.body()         # Get raw body as bytes
+        print("Raw Body (bytes):", raw_body)
+        print("Raw Body (string):", raw_body.decode("utf-8"))
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="An error occured")
     
