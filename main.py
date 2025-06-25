e5327abb38476edf9a27517b2b5ad89aaa65bea1
Ken Rolloque <Ken.Rolloque@fusionnode.ai>
2025-06-25 13:06:35 +0800

    Updated

--- main.py
+++ main.py
@@ -22,29 +22,28 @@ async def postAlert(
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Token")
     
         raw_body = await request.body()         # Get raw body as bytes
-        print(raw_body)
-        # decoded_body = raw_body.decode("utf-8")
+        decoded_body = raw_body.decode("utf-8")
         
-        # # print("Raw Body (bytes):", raw_body)
+        # print("Raw Body (bytes):", raw_body)
         # print("Raw Body (string):", raw_body.decode("utf-8"))
-        # try:
-        #     json_body = json.loads(decoded_body)
-        #     # print("📥 Received JSON (Pretty Printed):")
-        #     # print(json.dumps(json_body, indent=4))
+        try:
+            json_body = json.loads(decoded_body)
+            # print("📥 Received JSON (Pretty Printed):")
+            # print(json.dumps(json_body, indent=4))
             
-        #                 # Count logic here
-        #     if isinstance(json_body, list):
-        #         print(f"✅ JSON is a list with {len(json_body)} objects.")
-        #     elif isinstance(json_body, dict):
-        #         print(f"✅ JSON is a dict with {len(json_body)} top-level keys.")
-        #     else:
-        #         print("⚠️ JSON is a primitive type, not an object or array.")
+                        # Count logic here
+            if isinstance(json_body, list):
+                print(f"✅ JSON is a list with {len(json_body)} objects.")
+            elif isinstance(json_body, dict):
+                print(f"✅ JSON is a dict with {len(json_body)} top-level keys.")
+            else:
+                print("⚠️ JSON is a primitive type, not an object or array.")
                 
-        # except json.JSONDecodeError:
-        #     print("❌ Received non-JSON body:")
-        #     print(decoded_body)
+        except json.JSONDecodeError:
+            print("❌ Received non-JSON body:")
+            print(decoded_body)
 
-        # return {"status": "received"}
+        return {"status": "received"}
     except Exception as e:
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="An error occured")
     
